fb0fa0b89660dd92d58e3e40bca002e904125747
ObaraEmmanuel <emmanuelobarany@gmail.com>
2020-06-17 23:57:02 +0300

    Fixed ImageGrab issue when using pyinstaller

--- hoverset/platform/functions.py
+++ hoverset/platform/functions.py
@@ -11,9 +11,8 @@ from PIL import ImageGrab
 from hoverset.platform import platform_is, WINDOWS, MAC
 
 
-def image_grab(bbox=None, include_layered_windows=False, all_screens=False, childprocess=None, backend=None):
+def image_grab(bbox=None, childprocess=None, backend=None):
     if platform_is(WINDOWS) or platform_is(MAC):
-        return ImageGrab.grab(bbox, include_layered_windows, all_screens)
+        return ImageGrab.grab(bbox)
     else:
         return pyscreenshot.grab(bbox, childprocess, backend)
-
