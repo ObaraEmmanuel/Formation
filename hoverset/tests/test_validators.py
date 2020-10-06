555df2c927ca3f5d8964d8c9d0b305b881adc33c
ObaraEmmanuel <emmanuelobarany@gmail.com>
2020-10-06 19:13:32 +0300

    ADD: Added new validating utilities

---
+++ hoverset/tests/test_validators.py
@@ -0,0 +1,78 @@
+import unittest
+from hoverset.util.validators import *
+
+
+class ValidatorTestCase(unittest.TestCase):
+
+    def test_is_numeric(self):
+        self.assertFalse(is_numeric("some_text_43"))
+        self.assertFalse(is_numeric("2.0"))
+        self.assertTrue(is_numeric("6"))
+        self.assertTrue(is_numeric("+6"))
+        self.assertTrue(is_numeric("-6"))
+
+    def test_is_floating_numeric(self):
+        self.assertTrue(is_floating_numeric("6.90"))
+        self.assertTrue(is_floating_numeric("+6.70"))
+        self.assertTrue(is_floating_numeric("-6.6"))
+        self.assertTrue(is_floating_numeric("6"))
+
+    def test_is_identifier(self):
+        self.assertTrue(is_identifier("var45"))
+        self.assertFalse(is_identifier("from"))
+        self.assertFalse(is_identifier("45var"))
+
+    def test_is_signed(self):
+        self.assertFalse(is_signed("-rtpur"))
+        self.assertFalse(is_signed("--"))
+        self.assertFalse(is_signed("+6"))
+        self.assertTrue(is_signed("+"))
+        self.assertTrue(is_signed("-"))
+
+    def test_is_hex(self):
+        self.assertTrue(is_hex_color("#ff00ff"))
+        self.assertTrue(is_hex_color("#f"))
+        self.assertTrue(is_hex_color("#"))
+        self.assertTrue(is_hex_color("#abcdef"))
+        self.assertFalse(is_hex_color("#fgj"))
+        self.assertFalse(is_hex_color("d#k"))
+        self.assertFalse(is_hex_color("#fffffff"))
+
+    def test_test_limit(self):
+        self.assertTrue(limit(20, 300)("25.6"))
+        self.assertFalse(limit(20, 300)("20"))
+        self.assertFalse(limit(20, 300)("300"))
+        self.assertFalse(limit(20, 300)("2000"))
+        self.assertFalse(limit(20, 300)("gkgllk"))
+
+
+class TestCompositeValidators(unittest.TestCase):
+
+    def test_check_hex_color(self):
+        self.assertTrue(check_hex_color(""))
+        self.assertTrue(check_hex_color("#ff"))
+        self.assertTrue(check_hex_color("#"))
+
+    def test_numeric_limit(self):
+        self.assertTrue(numeric_limit("20", 15, 30))
+        self.assertFalse(numeric_limit("20.0", 15, 30))
+        self.assertFalse(numeric_limit("10", 15, 30))
+        self.assertFalse(numeric_limit("40", 15, 30))
+        self.assertFalse(numeric_limit("gkgkhk", 15, 30))
+
+
+class ChainingTestCase(unittest.TestCase):
+
+    def test_validate_all(self):
+        validator = validate_all(is_numeric, limit(20, 300))
+        self.assertFalse(validator("dfhlhld"))
+        self.assertFalse(validator("15"))
+        self.assertFalse(validator("400"))
+        self.assertTrue(validator("100"))
+
+    def test_validate_any(self):
+        validator = validate_any(is_empty, is_floating_numeric, is_hex_color)
+        self.assertTrue(validator("#ffe"))
+        self.assertTrue(validator(""))
+        self.assertTrue(validator("23.6"))
+        self.assertFalse(validator("fjlljd"))
--- hoverset/util/validators.py
+++ hoverset/util/validators.py
@@ -5,64 +5,116 @@ Ensure all validators here obey this fundamental design requirement!
 """
 
 import re
-import functools
+import keyword
 
 HEX_COLOR = re.compile(r"^#[0-9a-fA-F]{0,6}$")
+SIGNED = re.compile(r"^[+\-]$")
 
 
 # Most methods here are meant for use in realtime validation data entered through a user interface
 # For the sake of user experience you may need to pass empty value as valid
 # This accommodates for scenarios where the user deletes everything during typing to type a fresh value
-def empty_is_valid(func):
-
-    @functools.wraps(func)  # Pass the functions attributes to the returned wrapped function
-    def wrap(value: str, *args):
-        if value == "":
-            return True
-        return func(value, *args)
-
-    return wrap
+def is_empty(value: str) -> bool:
+    return value == ""
 
 
 # Enforce a numeric constraint to the validator
 # This returns false if the input is not numeric
-def is_numeric(func):
-
-    @functools.wraps(func)  # Pass the functions attributes to the returned wrapped function
-    def wrap(value: str, *args):
-        # Check if value can be converted to int
-        try:
-            int(value)
-            return func(value, *args)
-        except ValueError:
-            return False
-
-    return wrap
+def is_numeric(value: str) -> bool:
+    # Check if value can be converted to int
+    try:
+        int(value)
+        return True
+    except ValueError:
+        return False
 
 
-@empty_is_valid
-def check_hex_color(hex_color: str) -> bool:
+def is_floating_numeric(num: str) -> bool:
+    try:
+        float(num)
+        return True
+    except ValueError:
+        return False
+
+
+def is_signed(text: str) -> bool:
+    return bool(re.match(SIGNED, text))
+
+
+def is_identifier(text: str) -> bool:
+    """
+    Check whether a string can be used as a valid identifier. It returns false
+    if the string is an invalid identifier or is a python keyword
+    :param text:
+    :return:
+    """
+    return not keyword.iskeyword(text) and text.isidentifier()
+
+
+def validate_all(*validators):
+    """
+    returns a validator that evaluates to true only if all validators passed through
+    the argument validators also evaluate to true
+    :param validators: validators to be chained
+    :return: a validator function
+    """
+
+    def validator(value):
+        return all(v(value) for v in validators)
+
+    return validator
+
+
+def validate_any(*validators):
+    """
+    returns a validator that evaluates to true only if all validators passed through
+    the argument validators also evaluate to true
+    :param validators: validators to be chained
+    :return: a validator function
+    """
+
+    def chained_validator(value):
+        return any(v(value) for v in validators)
+
+    return chained_validator
+
+
+def is_hex_color(hex_color: str) -> bool:
     """
     Ensures values obey the format #xxxxxx where x is a hexadecimal value from 0 to f
     :param hex_color:
     :return: bool
     """
-    if re.match(HEX_COLOR, hex_color):
-        return True
-    return False
+    return bool(re.match(HEX_COLOR, hex_color))
 
 
-@empty_is_valid
-@is_numeric
-def numeric_limit(number, lower, upper):
+check_hex_color = validate_any(is_empty, is_hex_color)
+
+
+def limit(lower, upper):
     """
-    Multipurpose validator that ensures the number is within the stated lower and upper bounds
-    :param number: An int value to be tested
+    validator that ensures a floating point value is within the stated lower and upper bounds
     :param lower: An int value representing the lower bound below which the number is considered invalid
     :param upper:  An int value representing the upper bound above which the number is considered invalid
-    :return: bool
+    :return: a validator function for the given bounds
     """
+
     # No need to fear using int here since @is_numeric shields us from any non numeric values
-    if int(number) < lower or int(number) > upper:
+    def validator(number):
+        if is_floating_numeric(number):
+            return lower < float(number) < upper
         return False
-    return True
+
+    return validator
+
+
+def numeric_limit(value, upper, lower):
+    """
+    A limit validator adjusted to allow for leniency during typing by the user
+    while restricting the user to whole numbers
+    :param value: An int value to be tested
+    :param lower: An int value representing the lower bound below which the number is considered invalid
+    :param upper:  An int value representing the upper bound above which the number is considered invalid
+    :return: true if number lies within bounds
+    """
+    return validate_any(is_empty, validate_all(is_numeric, limit(upper, lower)))(value)
