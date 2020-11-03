3fcdef3096d6735228bff99ca80df13b7e8b2d3b
ObaraEmmanuel <emmanuelobarany@gmail.com>
2020-11-04 00:04:29 +0300

    Enhance dialog window focus behaviour

--- hoverset/ui/dialogs.py
+++ hoverset/ui/dialogs.py
@@ -11,8 +11,69 @@ from hoverset.ui.icons import get_icon_image
 
 class MessageDialog(Window):
     """
-    Main class for creation of hoverset themed message dialogs. It has the various initialization
-    methods for the common dialogs.
+    Main class for creation of hoverset themed alert dialogs. It has the various initialization
+    methods for the common dialogs. The supported forms/types are shown below:
+
+    .. _forms:
+
+        * OKAY_CANCEL     :py:meth:`MessageDialog.ask_okay_cancel`
+        * YES_NO          :py:meth:`MessageDialog.ask_question`
+        * RETRY_CANCEL    :py:meth:`MessageDialog.ask_retry_cancel`
+        * SHOW_PROGRESS   :py:meth:`MessageDialog.show_progress`
+        * SHOW_WARNING    :py:meth:`MessageDialog.show_warning`
+        * SHOW_ERROR      :py:meth:`MessageDialog.show_error`
+        * SHOW_INFO       :py:meth:`MessageDialog.show_info`
+
+    There are three icons used in the dialogs:
+
+    .. _icons:
+
+        * :py:attr:`MessageDialog.ICON_ERROR`
+        * :py:attr:`MessageDialog.ICON_INFO`
+        * :py:attr:`MessageDialog.ICON_WARNING`
+
+    Some dialogs return values while others just notify. Below illustrates using the various dialogs
+
+    .. code-block:: python
+
+        # assuming we have a hoverset Application object app
+        # program will wait for value to be obtained
+
+        val = MessageDialog.ask_retry_cancel(
+            title="ask_okay",
+            message="This is an ask-okay-cancel message",
+            parent=app
+        )
+
+        # do whatever you need with the value, in this case let's just print the response
+
+        if val == True:
+            print("retry")
+        elif val == False:
+            print("cancel")
+        elif val is None:
+            print("no value selected")
+
+        # show dialogs don't need to return values
+        MessageDialog.show_error(
+            title="Error",
+            message="This is an error message",
+            parent=app
+        )
+
+        val = MessageDialog.builder(
+            # define the buttons
+            {"text": "Continue", "value": "continue", "focus": True},
+            {"text": "Pause", "value": "pause"},
+            {"text": "Cancel", "value": False},
+            wait=True,
+            title="Builder",
+            message="We just built this dialog from scratch",
+            parent=app,
+            icon="flame"
+        )
+        print(val)
+        # prints 'continue', 'pause' or False depending on the value of button clicked
     """
 
     ICON_ERROR = "dialog_error"
@@ -32,6 +93,7 @@ class MessageDialog(Window):
     DETERMINATE = ProgressBar.DETERMINATE
 
     _MIN_BUTTON_WIDTH = 60
+    _MAX_SHAKES = 10
 
     def __init__(self, master, render_routine=None, **kw):
         super().__init__(master)
@@ -56,11 +118,46 @@ class MessageDialog(Window):
         }
         if render_routine in routines:
             # Completely custom dialogs
-            routines.get(render_routine)(**kw)
+            routines[render_routine](**kw)
         elif render_routine is not None:
             render_routine(self)
         self.enable_centering()
         self.value = None
+        self.bind('<Visibility>', lambda _: self.grab_set())
+        self.bind('<FocusOut>', lambda _: self.grab_release())
+        self.bind('<FocusIn>', lambda _: self.grab_set())
+        self._reaction = -1
+        self.bind('<Button>', self._on_event)
+
+    def _react(self, step):
+        """
+        Shake the dialog window to gain user attention
+
+        :param step: displacement in pixels, use negative values for
+            left displacement and positive for right displacement
+        """
+        # a value of -1 in self._reaction is a sentinel indicating no reaction is ongoing
+        if self._reaction > MessageDialog._MAX_SHAKES:
+            self._reaction = -1
+            return
+        try:
+            x, y = self.winfo_x() + step, self.winfo_y()
+            self.geometry('+{}+{}'.format(x, y))
+            self.after(100, lambda: self._react(step * -1))
+            self._reaction += 1
+        except Exception:
+            self._reaction = -1
+            pass
+
+    def _on_event(self, event):
+        if not self.event_in(event, self):
+            # the user tried to click outside the focused dialog window
+            # we'll try to bring their attention back to the dialog
+            self.bell()
+            if self._reaction < 0:
+                # displace by 2 pixels on each shake
+                # use larger value for more pronounced displacement
+                self._react(2)
 
     def _make_button_bar(self):
         self.bar = Frame(self, **self.style.dark, **self.style.dark_highlight_dim)
@@ -128,7 +225,7 @@ class MessageDialog(Window):
         text = kw.get('message', 'progress')
         icon = None
         if kw.get('icon'):
-            icon = get_icon_image(icon, 50, 50)
+            icon = get_icon_image(kw.get('icon'), 50, 50)
         Label(self, **self.style.dark_text,
               text=text, anchor="w", compound="left", wrap=600, justify="left",
               pady=5, padx=15, image=icon
@@ -153,14 +250,39 @@ class MessageDialog(Window):
     @classmethod
     def ask(cls, form, **kw):
         """
-        Create a dialog which returns a value when complete. You do not have to use this
-        method directly since there are specialized methods for instance
-        instead of -> use this
-        MessageDialog.ask(MessageDialog.OKAY_CANCEL, ...) -> MessageDialog.ask_okay_cancel(...)
-        MessageDialog.ask(MessageDialog.SHOW_INFO, ...) -> MessageDialog.show_info(...)
-        :param form:
-        :param kw: The keywords arguments included
-        :return:
+        General method for creation common dialogs. You do not have to use this
+        method directly since there are specialized methods as shown below.
+
+        .. table::
+            :align: center
+
+            ===================================================  ==================================
+            instead of                                           use this
+            ===================================================  ==================================
+            MessageDialog.ask(MessageDialog.OKAY_CANCEL, ...)    MessageDialog.ask_okay_cancel(...)
+            MessageDialog.ask(MessageDialog.RETRY_CANCEL, ...)   MessageDialog.ask_retry_cancel(...)
+            MessageDialog.ask(MessageDialog.SHOW_INFO, ...)      MessageDialog.show_info(...)
+            MessageDialog.ask(MessageDialog.SHOW_ERROR, ...)     MessageDialog.show_error(...)
+            MessageDialog.ask(MessageDialog.SHOW_PROGRESS, ...)  MessageDialog.show_progress(...)
+            MessageDialog.ask(MessageDialog.SHOW_WARNING, ...)   MessageDialog.show_warning(...)
+            MessageDialog.ask(MessageDialog.YES_NO, ...)         MessageDialog.ask_question(...)
+            ===================================================  ==================================
+
+        :param form: The type of dialog to be created as defined in forms_ above
+        :param kw: The keywords arguments included. These are the common arguments:
+
+            .. _common_args:
+
+                * **parent**: (Required) A hoverset based toplevel widget such as :class:`hoverset.ui.widgets.Application`
+                * **title**: Title to be used for the dialog window
+                * **message**: Message to be displayed in the alert dialog
+                * **icon**: Icon to be displayed in the dialog. Should be one of these icons_.
+
+            .. warning::
+                The ``parent`` argument is mandatory and should always be provided. Absence of the parent
+                argument will result in missing theme style definitions and cause errors.
+
+        :return: A value or ``None`` depending on the dialog type. See specialized method for mor details
         """
         parent = kw.get("parent")
         dialog = MessageDialog(parent, form, **kw)
@@ -169,39 +291,123 @@ class MessageDialog(Window):
 
     @classmethod
     def ask_okay_cancel(cls, **kw):
+        """
+        Show a dialog windows with two buttons: ``okay`` and ``cancel``
+
+        :param kw: Keyword arguments as defined in common_args_.
+        :return: Returns ``True`` if "okay" is selected, ``False`` if "cancel" is selected or
+            ``None`` if no choice is selected
+        """
         return cls.ask(MessageDialog.OKAY_CANCEL, **kw)
 
     @classmethod
     def ask_question(cls, **kw):
+        """
+        Show a dialog window with two button: ``yes`` and ``no``
+
+        :param kw: Keyword arguments as defined in common_args_.
+        :return: Returns ``True`` if "yes" is selected, ``False`` if "no" is selected or
+            ``None`` if no choice is selected
+        """
         return cls.ask(MessageDialog.YES_NO, **kw)
 
     @classmethod
     def ask_retry_cancel(cls, **kw):
+        """
+        Show a dialog window with two buttons: ``retry`` and ``cancel``
+
+        :param kw: Keyword arguments as defined in common_args_.
+        :return: Returns ``True`` if "retry" is selected, ``False`` if "cancel" is selected or
+            ``None`` if no choice is selected
+        """
         return cls.ask(MessageDialog.RETRY_CANCEL, **kw)
 
     @classmethod
     def show_error(cls, **kw):
+        """
+        Show an error message with an error icon.
+
+        :param kw: Keyword arguments as defined in common_args_.
+        :return: ``None``
+        """
         parent = kw.get("parent")
         cls(parent, MessageDialog.SHOW_ERROR, **kw)
 
     @classmethod
     def show_warning(cls, **kw):
+        """
+        Show an warning message with a warning icon.
+
+        :param kw: Keyword arguments as defined in common_args_.
+        :return: ``None``
+        """
         parent = kw.get("parent")
         cls(parent, MessageDialog.SHOW_WARNING, **kw)
 
     @classmethod
     def show_info(cls, **kw):
+        """
+        Show an info message with an info icon.
+
+        :param kw: Keyword arguments as defined in common_args_.
+        :return: ``None``
+        """
         parent = kw.get("parent")
         cls(parent, MessageDialog.SHOW_INFO, **kw)
 
     @classmethod
     def show_progress(cls, **kw):
+        """
+
+        :param kw: Config options for the progress dialog
+
+            * **parent**: A hoverset based toplevel widget such as :class:`hoverset.ui.widgets.Application`
+            * **title**: Title to be used for the dialog window
+            * **message**: Message to be displayed in the alert dialog
+            * **icon**: Icon to be displayed in the dialog. Should be one of these icons_.
+            * **mode**: One of the two modes
+
+                * :py:attr:`MessageDialog.DETERMINATE`
+                * :py:attr:`MessageDialog.INDETERMINATE`
+
+            * **color**: Color to be used for the progressbar
+            * **interval**: The update interval in :py:attr:`MessageDialog.INDETERMINATE` in milliseconds.
+              The smaller the interval the faster the animation.
+
+        :return: The dialog window. The underlying progressbar can then be accessed through
+            the property :py:attr:`MessageDialog.progress`. The progressbar is
+            a :class:`hoverset.ui.widgets.ProgressBar` object and can be updated as required
+        """
         parent = kw.get("parent")
         dialog = cls(parent, MessageDialog.SHOW_PROGRESS, **kw)
         return dialog
 
     @classmethod
     def builder(cls, *buttons, **kw):
+        """
+        Create custom dialogs with custom buttons, icons and return values. An example of the use of a
+        builder has been provided at the beginning of this page.
+
+        :param buttons: A tuple containing a dictionary defining the custom buttons with the following keys
+
+            * **text**: Text to be displayed in the button
+            * **value**: Value returned when button is clicked
+            * **focus**: Whether to focus on button when dialog is displayed. Only a single
+              button should have ``focus`` set to ``True``
+
+        :param kw: config options for the builder:
+
+            * **parent**: A hoverset based toplevel widget such as :class:`hoverset.ui.widgets.Application`
+            * **title**: Title to be used for the dialog window
+            * **message**: Message to be displayed in the alert dialog
+            * **icon**: Icon to be displayed in the dialog. Should be one of these icons_.
+            * **wait**: Set to ``True`` to suspend the program and wait a value to be returned or
+              ``False`` to just continue program execution without waiting for a value. Useful when you just need
+              to display a message
+
+        :return: A custom value depending on the custom button clicked. If ``wait = False`` no value is returned.
+            If no value is selected by the user ``None`` is returned
+        """
         parent = kw.get("parent")
         kw["actions"] = buttons
         dialog = cls(parent, MessageDialog.BUILDER, **kw)
