65d8b058ea543fffff9f6cbe5f9d596f9124bbea
ObaraEmmanuel <emmanuelobarany@gmail.com>
2020-12-17 23:27:28 +0300

    Add shortcut editing utilities

--- hoverset/data/actions.py
+++ hoverset/data/actions.py
@@ -7,6 +7,18 @@ A store and manager for all application functions and routines
 
 
 class Routine:
+    """
+    Representation of an action available throughout the lifetime
+    of a hoverset application
+
+    * **key**: A uniques string value used to access the action
+    * **desc**: Long description of what the action does
+    * **group**: group to which the action belongs. Can be ``None``
+    * **shortcut**: A :py:class:`hoverset.data.keymap.Key` representing
+      shortcut that can invoke the action
+    * **func**: The actual function the action invokes
+
+    """
     def __init__(self, func, key, desc, group=None, shortcut=None):
         self.key = key
         self.desc = desc
@@ -16,6 +28,12 @@ class Routine:
 
     @property
     def accelerator(self):
+        """
+        Label for the shortcut key that invokes the action
+
+        :return: String representing the shortcut combination that invokes
+          routine
+        """
         # return the shortcut key combination for display
         if self.shortcut:
             return self.shortcut.label
@@ -29,11 +47,22 @@ _all_actions = {}
 
 
 def add(*routines):
+    """
+    Add routines
+
+    :param routines: :py:class:`Routine` objects to be added
+    """
     for routine in routines:
         _all_actions[routine.key] = routine
 
 
 def get_routine(key):
+    """
+    Get a routine with given key
+
+    :param key: String key for routine to be obtained
+    :return: :py:class:`Routine` object with given key
+    """
     return _all_actions.get(key)
 
 
@@ -41,5 +70,32 @@ get = get_routine  # Shorter method get
 
 
 def remove(*routines):
+    """
+    Remove routines from global map
+
+    :param routines: routines to be removed
+    """
     for routine in routines:
         _all_actions[routine.key] = routine
+
+
+def all_routines():
+    """
+    Get dictionary of all routines
+
+    :return: A dictionary with string keys and :py:class`Routine` values
+    """
+    return _all_actions
+
+
+def routine_from_shortcut(shortcut):
+    """
+    Get routine object with given shortcut
+
+    :param shortcut: :py:class:`hoverset.data.keymap.Key` object
+    :return: Routine object with given shortcut otherwise None
+    """
+    search = list(filter(lambda r: r.shortcut == shortcut, _all_actions.values()))
+    if len(search) > 0:
+        return search[0]
+    return None
--- hoverset/data/keymap.py
+++ hoverset/data/keymap.py
@@ -3,7 +3,11 @@
 # ======================================================================= #
 
 import hoverset.data.actions as actions
-from hoverset.data.preferences import SharedPreferences
+from hoverset.data.preferences import SharedPreferences, Component
+from hoverset.data.images import get_tk_image
+from hoverset.ui.widgets import Frame, Label, CompoundList
+from hoverset.ui.dialogs import MessageDialog
+from hoverset.ui.menu import EnableIf
 
 
 class Key:
@@ -29,6 +33,9 @@ class Key:
     @property
     def keycode(self):
         return self._keycodes
+
+
+BlankKey = Key('')
 
 
 class CharKey(Key):
@@ -157,15 +164,18 @@ class KeyMap(metaclass=_KeymapDispatch):
         # just call the routine directly since this is a simplified keymap
         routine_key()
 
-    def get_key(self, event):
+    @classmethod
+    def get_key(cls, event):
         key = None
-        for mod in self.EVENT_MASK:
+        for mod in cls.EVENT_MASK:
             if event.state & mod:
                 if key is None:
-                    key = self.EVENT_MASK[mod]
+                    key = cls.EVENT_MASK[mod]
                 else:
-                    key += self.EVENT_MASK[mod]
-        main_key = Key(chr(event.keycode), event.keycode)
+                    key += cls.EVENT_MASK[mod]
+        # key symbols one character long are best displayed in upper case
+        sym = event.keysym.upper() if len(event.keysym) == 1 else event.keysym
+        main_key = Key(sym, event.keycode)
         key = main_key if key is None else key + main_key
         return key
 
@@ -188,18 +198,21 @@ class ShortcutManager(KeyMap):
     """
     Extended Keymap that fetches bindings from a config file and
     implicitly handles Routine - shortcut mapping
+
+    :param window: A tkinter Toplevel window
+    :param preferences: A SharedPreference object where bindings data is
+        to be retrieved from
     """
 
+    instances = []
+
     def __init__(self, window, preferences: SharedPreferences):
-        """
-        Shortcut manager for a given window
-        :param window: A tkinter Toplevel window
-        :param preferences: A SharedPreference object
-        """
         super().__init__(window)
-        preferences.set('hotkeys', {})
-        self.bindings = preferences.get('hotkeys')
-        self.reversed_bindings = {value: key for key, value in self.bindings.items()}
+        ShortcutManager.instances.append(self)
+        preferences.set_default('allow_hotkeys', True)
+        preferences.set_default('hotkeys', {})
+        self.preferences = preferences
+        self.update_bindings()
 
     def add_routines(self, *routines):
         """
@@ -212,7 +225,7 @@ class ShortcutManager(KeyMap):
             actions.add(routine)
             if routine.shortcut is None:
                 continue
-            if routine.shortcut in self.bindings:
+            if routine.key in self.reversed_bindings:
                 # if shortcut is already set in bindings just ignore
                 # setting shortcuts at this point is only important as defaults
                 # just in case we have never bound that shortcut
@@ -220,6 +233,11 @@ class ShortcutManager(KeyMap):
                 continue
             self.bindings[routine.shortcut] = routine.key
             self.reversed_bindings[routine.key] = routine.shortcut
+
+    def _dispatch(self, event):
+        # allow dispatch if and only if hotkeys are allowed
+        if self.preferences.get("allow_hotkeys"):
+            super()._dispatch(event)
 
     def _invoke(self, routine_key):
         # Things get slightly complicated, we need to use the key to
@@ -232,5 +250,206 @@ class ShortcutManager(KeyMap):
     def get_shortcut(self, routine_key):
         return self.reversed_bindings.get(routine_key)
 
+    def update_bindings(self):
+        # update binding dictionary with latest info
+        # Bindings are stored with actions as keys since action keys are unique
+        self.reversed_bindings = self.preferences.get('hotkeys')
+        # For faster routing we use shortcut keys as the dict keys so we reverse
+        self.bindings = {value: key for key, value in self.reversed_bindings.items()}
+
     def add_shortcut(self, handler, shortcut):
+        # this method doesn't work since shortcuts are fetched from preferences
         raise NotImplementedError('This method does not work, use add_routine instead or use a KeyMap object')
+
+
+class ShortcutPicker(MessageDialog):
+
+    def __init__(self, master, message, shortcut_pane=None):
+        self.message = message
+        super().__init__(master, self.render)
+        self.title("Shortcut Picker")
+        self.resizable(0, 0)
+        self.geometry('350x200')
+        self.value = None
+        self.key = None
+        self.shortcut_pane = shortcut_pane
+
+    def on_key_change(self, event):
+        self.key = KeyMap.get_key(event)
+        if self.shortcut_pane is not None:
+            routine = self.shortcut_pane.routine_from_shortcut(self.key)
+            if routine is not None and self.shortcut_pane:
+                self._warning['text'] = f"Key already assigned to {routine.desc}"
+                self._warning.pack(fill="x")
+            else:
+                self._warning.pack_forget()
+        self.event_pad.config(text=self.key.label)
+        # returning break ensures this event does not propagate
+        # preventing the event from invoking currently set bindings
+        return "break"
+
+    def render(self, _):
+        self.detail = Label(self, **self.style.dark_text, text=self.message)
+        self.detail.pack(fill="x")
+        warn_frame = Frame(self, **self.style.dark)
+        self._warning = Label(
+            warn_frame,
+            **self.style.dark_text_passive,
+            padx=5,
+            anchor='w',
+            compound="left",
+            image=get_tk_image("dialog_warning", 15, 15),
+        )
+        self.event_pad = Label(
+            self, **self.style.dark_text_accent)
+        self._add_button(text="Cancel", value=None)
+        self._add_button(text="Okay", command=self.exit_with_key, focus=True)
+        warn_frame.pack(side="bottom", fill="x")
+        self.event_pad.config(
+            **self.style.bright, takefocus=True,
+            text="Tap here to begin capturing shortcuts."
+        )
+        self.event_pad.bind("<Any-KeyPress>", self.on_key_change)
+        self.event_pad.bind("<Button-1>", lambda e: self.event_pad.focus_set())
+        self.event_pad.pack(fill="both", expand=True)
+
+    def exit_with_key(self, _):
+        self.value = self.key
+        self.destroy()
+
+    @classmethod
+    def pick(cls, master, message, shortcut_pane=None):
+        picker = cls(master, message, shortcut_pane)
+        picker.wait_window()
+        return picker.value
+
+
+class ShortcutPane(Component, Frame):
+    class ShortcutItem(CompoundList.BaseItem):
+
+        def __init__(self, master, value, index, isolated=False):
+            super().__init__(master, value, index, isolated)
+            initial_key = value[1]
+            self.key = initial_key
+
+        def set_key(self, key):
+            self.key_label.config(text=key.label)
+            self.key = key
+            self._value = (self.value[0], key)
+
+        def render(self):
+            self.key_label = Label(
+                self, text=self.value[1].label, **self.style.dark_text_accent
+            )
+            self.key_label.pack(side="right")
+            routine = actions.get_routine(self.value[0])
+            self.desc = Label(
+                self, text=routine.desc, **self.style.dark_text
+            )
+            self.desc.pack(side="left")
+
+        def on_hover_ended(self, *_):
+            self.config_all(**self.style.dark)
+
+        def on_hover(self, *_):
+            self.config_all(**self.style.bright)
+
+        def disable(self, flag):
+            self.disabled(flag)
+            if flag:
+                self.desc.config(**self.style.dark_text_passive)
+                self.key_label.config(**self.style.dark_text_passive)
+            else:
+                self.desc.config(**self.style.dark_text)
+                self.key_label.config(**self.style.dark_text_accent)
+
+    def __init__(self, master, pref: SharedPreferences, path, _, **__):
+        super().__init__(master)
+        self.config(**self.style.dark)
+        self.bindings = {}
+        self.is_disabled = False
+        self.load(pref, path)
+        self.shortcut_list = CompoundList(self)
+        self.shortcut_list.set_item_class(ShortcutPane.ShortcutItem)
+        self.shortcut_list.set_values(list(self.bindings.items()))
+        self.shortcut_list.pack(fill="both", expand=True)
+        self.shortcut_list.body.set_up_context((
+            EnableIf(
+                lambda: (not self.is_disabled) and self.shortcut_list.get(),
+                ("command", "Change Shortcut", get_tk_image('edit', 14, 14), self.pick_key, {}),
+                EnableIf(
+                    lambda: self.shortcut_list.get() and self.shortcut_list.get().value[1] != BlankKey,
+                    ("command", "Remove", get_tk_image('delete', 14, 14), self.remove_key, {})
+                ),
+            ),
+        ))
+
+    def remove_key(self):
+        if self.shortcut_list.get():
+            item = self.shortcut_list.get()
+            routine = actions.get_routine(item.value[0])
+            self.bindings[routine.key] = BlankKey
+            item.set_key(BlankKey)
+
+    def get_item(self, key):
+        items = list(filter(
+            lambda i: i.value[1] == key,
+            self.shortcut_list.items
+        ))
+        if len(items):
+            return items[0]
+        return None
+
+    def routine_from_shortcut(self, shortcut):
+        search = list(filter(
+            lambda r: self.bindings[r] == shortcut,
+            self.bindings))
+        if len(search) > 0:
+            return actions.get_routine(search[0])
+        return None
+
+    def pick_key(self):
+        if self.shortcut_list.get():
+            item = self.shortcut_list.get()
+            routine = actions.get_routine(item.value[0])
+            key = ShortcutPicker.pick(
+                self.window,
+                routine.desc,
+                self
+            )
+            if key:
+                # overwrite any present bindings by replacing with blank key
+                overwritten = self.routine_from_shortcut(key)
+                overwritten_item = self.get_item(key)
+                if overwritten:
+                    self.bindings[overwritten.key] = BlankKey
+                    if overwritten_item:
+                        overwritten_item.set_key(BlankKey)
+                self.bindings[routine.key] = key
+                item.set_key(key)
+
+    def _add_key(self, key: Key):
+        pass
+
+    def disable(self, flag):
+        self.is_disabled = flag
+        for item in self.shortcut_list.items:
+            item.disable(flag)
+
+    def get(self):
+        return self.bindings
+
+    def set(self, value):
+        if isinstance(value, dict):
+            self.bindings = dict(value.items())
+        else:
+            raise ValueError("Value must be a dictionary")
+
+    def commit(self):
+        super().commit()
+        for action, key in self.bindings.items():
+            routine = actions.get_routine(action)
+            if routine:
+                routine.shortcut = key
+        for instance in ShortcutManager.instances:
+            instance.update_bindings()
