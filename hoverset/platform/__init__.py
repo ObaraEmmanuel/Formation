6ac24640b00d0607918624b75100c86cb71d467f
ObaraEmmanuel <emmanuelobarany@gmail.com>
2021-05-29 22:23:18 +0300

    Add windowing system check utility

--- hoverset/platform/__init__.py
+++ hoverset/platform/__init__.py
@@ -14,10 +14,42 @@ ATHEOS = 'atheos'
 FREEBSD = 'freebsd'
 OPENBSD = 'openbsd'
 
+# windowing systems
+
+X11 = 'x11'
+AQUA = 'aqua'
+WIN32 = 'win32'
+
+
+def windowing_is(root, *window_sys):
+    """
+    Check for the current operating system.
+
+    :param root: A tk widget to be used as reference
+    :param window_sys: if any windowing system provided here is the current
+    windowing  system `True` is returned else `False`
+
+    :return: boolean
+    """
+    windowing = root.tk.call('tk', 'windowingsystem')
+    return windowing in window_sys
+
+
+def is_threaded_build(root):
+    """
+    Check whether the tk build in use supports threading.
+
+    :param root: the tk widget to be used as reference
+
+    :return: ``1`` if threading is supported ``0`` otherwise
+    """
+    return int(root.tk.eval("set tcl_platform(threaded)"))
+
 
 def platform_is(*platforms) -> bool:
     """
     Check for the current operating system.
+
     :param platforms: if any platform provided here is the current
     operating system `True` is returned else `False`
 
