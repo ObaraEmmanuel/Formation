f5a27a611b65c68332fab99789fe65ca887d7a97
ObaraEmmanuel <emmanuelobarany@gmail.com>
2022-08-16 20:06:16 +0300

    Embed actual toplevel widget for more realistic design preview

--- studio/lib/toplevel.py
+++ studio/lib/toplevel.py
@@ -1,14 +1,15 @@
+# ======================================================================= #
+# Copyright (c) 2022 Hoverset Group.                                      #
+# ======================================================================= #
+
 import tkinter as tk
 
 from hoverset.data.images import get_tk_image
 from studio.lib.pseudo import Groups, Container
 
 
-class _Toplevel(Container, tk.Frame):
-    group = Groups.container
-    icon = 'window'
-    is_toplevel = True
-
+class _Toplevel(tk.Frame):
+    impl = tk.Toplevel
     _images = None
 
     def __init__(self, master, id_):
@@ -36,27 +37,61 @@ class _Toplevel(Container, tk.Frame):
         tk.Label(self.title, image=self._images[1]).pack(side="right", padx=10, pady=5)
 
         # body has to be sibling of toplevel for positioning to work
-        self._body = tk.Frame(master, bg="#bbbbbb")
+        body = tk.Frame(master, bg="#bbbbbb", container=True)
+        body.winfo_parent = lambda: str(self)
+        body.grid(in_=self, row=1, column=0, sticky='nswe')
+
+        # embed an actual toplevel widget
+        if issubclass(self.impl, tk.Toplevel):
+            self._toplevel = self.impl(master, use=body.winfo_id())
+        else:
+            self._toplevel = self.impl(use=str(body.winfo_id()))
         # make body appear as child to toplevel
-        self._body.winfo_parent = lambda: str(self)
-        self._body.grid(in_=self, row=1, column=0, sticky='nswe')
+        self._toplevel.winfo_parent = lambda: str(self)
+        self._toplevel.configure(bg="#bbbbbb")
 
         self.setup_widget()
-        self.body = self._body
+        self.body = self._toplevel
+
+    def keys(self):
+        return self._toplevel.keys()
+
+    def configure(self, **kwargs):
+        conf = self._toplevel.configure(**kwargs)
+        if "menu" in kwargs:
+            self.update_idletasks()
+        return conf
+
+    config = configure
+
+    def __getitem__(self, item):
+        return self._toplevel[item]
+
+    def __setitem__(self, key, value):
+        self._toplevel[key] = value
+
+    def cget(self, key):
+        return self._toplevel.cget(key)
 
     def set_name(self, name):
         self.label["text"] = f"  {name}"
 
-    def bind_all(self, sequence, func=None, add=None):
-        super(_Toplevel, self).bind_all(sequence, func, add)
-        self._body.bind(sequence, func, add)
+    def bind(self, sequence=None, func=None, add=None):
+        super(_Toplevel, self).bind(sequence, func, add)
+        self._toplevel.bind(sequence, func, add)
 
 
-class Toplevel(_Toplevel):
+class Toplevel(Container, _Toplevel):
+    group = Groups.container
+    icon = 'window'
+    is_toplevel = True
     display_name = 'Toplevel'
     impl = tk.Toplevel
 
 
-class Tk(_Toplevel):
+class Tk(Container, _Toplevel):
+    group = Groups.container
+    icon = 'window'
+    is_toplevel = True
     display_name = 'Tk'
     impl = tk.Tk
