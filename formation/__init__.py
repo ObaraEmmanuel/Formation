8c164e4c1a66c037a9166ab85ae74520d0a3905b
ObaraEmmanuel <emmanuelobarany@gmail.com>
2023-06-07 13:59:21 +0300

    bump version to 0.6.1

--- formation/__init__.py
+++ formation/__init__.py
@@ -1,3 +1,3 @@
-__version__ = "0.6.0"
+__version__ = "0.6.1"
 
 from .loader import Builder, AppBuilder  # noqa
--- studio/__init__.py
+++ studio/__init__.py
@@ -1,4 +1,4 @@
-__version__ = "0.6.0"
+__version__ = "0.6.1"
 
 # make available for use with custom widgets
 from studio.lib.pseudo import WidgetMeta
