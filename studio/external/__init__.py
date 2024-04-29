915efc80dd79e000111b54f9a6a02df5dba30305
ObaraEmmanuel <emmanuelobarany@gmail.com>
2024-04-29 23:45:01 +0300

    Add proper error handling for loading third party features

--- studio/external/__init__.py
+++ studio/external/__init__.py
@@ -1,6 +1,8 @@
 import os
 import logging
 
+from studio.external._base import FeatureNotAvailableError
+
 from hoverset.util.execution import import_path
 
 
@@ -8,12 +10,14 @@ def init_externals(studio):
 
     external = os.path.dirname(__file__)
     for module_path in os.listdir(external):
-        if module_path.endswith(".py") and module_path != "__init__.py":
+        if module_path.endswith(".py") and not module_path.startswith("_"):
             try:
                 module = import_path(os.path.join(external, module_path))
+                if hasattr(module, "init"):
+                    module.init(studio)
+            except FeatureNotAvailableError:
+                # feature is probably not installed
+                pass
             except Exception as e:
                 logging.error(f"Failed to load external module {module_path}: {e}")
                 continue
-
-            if hasattr(module, "init"):
-                module.init(studio)
---
+++ studio/external/_base.py
@@ -0,0 +1,3 @@
+class FeatureNotAvailableError(Exception):
+    """Exception raised when a feature is not available."""
+    pass
