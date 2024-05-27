d815d553bcb85a61bced748cc49e880553f2699e
KavyanshKhaitan2 <73186427+KavyanshKhaitan2@users.noreply.github.com>
2024-05-27 17:48:02 +0530

    Update callback_parse.py
    changed camelCase to snake_case for consistency. Also, removed unnecessary functions.

--- formation/callback_parse.py
+++ formation/callback_parse.py
@@ -13,85 +13,38 @@ import typing
 
 STRING_DELIMETERS = ['"', "'"]
 
-def isValidArg(arg:str) -> str:
-    """Checks if an argument is valid.
-
-    Args:
-        arg (str): Raw argument to check.
-
-    Returns:
-        bool: Is the argument valid?
-    """
-    if not arg: return False
-    
-    for x in STRING_DELIMETERS:
-        if [arg[0], arg[-1]] == [x, x]:
-            return True
-    if [arg[0] not in STRING_DELIMETERS, arg[-1] not in STRING_DELIMETERS] == [True, True]:
-        return True
-    if arg.find('=') != -1:
-        eqpos = arg.find('=')
-        value = arg[eqpos+1:]
-        if isValidArg(value):
-            return True
-    return False
-
-def processArg(arg:str, parsedCommandList:list[str], commandKwargs:dict[str, typing.Any]):
-    """Process an argument. Dependency of `parse` method.
-
-    Args:
-        `arg (str) : . . . . . . . . . `Raw argument in string to parse.
-        `parsedCommandList (list): . . `List of parsed raw arguments in strings. (WILL be modified!)
-        `commandKwargs (dict): . . . . `List of parsed raw keyword arguments in strings. (WILL be modified!)
-    """
-    if arg.find("=") != -1:
-        inString = 0
-        for char in arg:
-            if char in STRING_DELIMETERS:
-                inString = 1 - inString
-            if char == "=":
-                break
-        if not inString:
-            eqpos = arg.find("=")
-            key = arg[:eqpos]
-            value = arg[eqpos+1:]
-            
-            commandKwargs[key] = value
-            return
-    parsedCommandList.append(arg)
-
-def parseHelper(*args, **kwargs):
+def parse_helper(*args, **kwargs):
     return args, kwargs
 
 def parse(command:str):
     """Returns parts of a command after parsing it using eval method.
 
     Args:
-        `command (str): . . ` command string.
+        ``command (str): . . `` command string.
     
     Command String Syntax:
-    	`funcname 1 "arg2",kwarg1="hello",kwarg2="world!"`
+    	``funcname 1 "arg2",kwarg1="hello",kwarg2="world!"``
 
     Returns:
-        `commandFunc (str) : . . . . `name of the function.
-        `commandArgs (list): . . . . `values of regular arguments.
-        `commandKwargs (dict): . . . `values of kwargs.
+        ``command_func (str) : . . . . ``name of the function.
+        ``command_args (list): . . . . ``values of regular arguments.
+        ``command_kwargs (dict): . . . ``values of kwargs.
     """
-    commandList:list = command.split(' ')
-    commandFunc: str = commandList[0]
-    commandList.pop(0)
-    commandString = ""
-    for x in commandList:
-        commandString+=(x+' ')
-    commandString.removesuffix(' ')
-    args, kwargs = eval(f'parseHelper({commandString})')
-    return commandFunc, args, kwargs
+    command_list:list = command.split(' ')
+    command_func: str = command_list[0]
+    command_list.pop(0)
+    command_string = ""
+    for arg in command_list:
+        command_string+=(arg+' ')
+    command_string.removesuffix(' ')
+    args, kwargs = eval(f'parse_helper({command_string})')
+    return command_func, args, kwargs
 
 # if __name__ == "__main__":
-#     command = "btn 1, 2, 3, 'hello this is a world!', kw1='myKwarg'"
+#     command = "btn 1, 2, 3, 'hello this is a world!', kw1='my kwarg'"
 #     output = parse(command)
-#     commandFunc, commandArgs, commandKwargs = output
-#     print(f"{commandKwargs = }")
-#     print(f"{commandArgs = }")
-#     print(f"{commandFunc = }")
+#     command_func, command_args, command_kwargs = output
+#     print(f"{command_kwargs = }")
+#     print(f"{command_args = }")
+#     print(f"{command_func = }")
 
