5c0f49513a4a08e1ed07a607393b99d466f37354
ObaraEmmanuel <emmanuelobarany@gmail.com>
2025-05-06 22:43:08 +0300

    [STUDIO] Bump version to 0.8.16

--- formation/__init__.py
+++ formation/__init__.py
@@ -1,3 +1,3 @@
-__version__ = "0.8.15"
+__version__ = "0.8.16"
 
 from .loader import Builder, AppBuilder  # noqa
--- studio/__init__.py
+++ studio/__init__.py
@@ -1,5 +1,4 @@
-__version__ = "0.8.15"
-# TODO: remove Labour Day decals on next release
+__version__ = "0.8.16"
 
 # make available for use with custom widgets
 from studio.lib.pseudo import WidgetMeta
