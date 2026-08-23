c48ce852b57776a0ca4d0a4c9347d76b9ec7f6c8
ObaraEmmanuel <emmanuelobarany@gmail.com>
2026-08-24 02:25:35 +0300

    [STUDIO] Undo breaking changes to widget selection target layout selection mechanics

--- studio/feature/design.py
+++ studio/feature/design.py
@@ -599,7 +599,7 @@ class Designer(DesignPad, Container):
     def layout_at(self, bounds):
         candidate = None
         for container in self._sorted_containers:
-            if geometry.compute_overlap(geometry.bounds(container), bounds):
+            if geometry.is_within(geometry.bounds(container), bounds):
                 candidate = container
                 break
         if self.current_container and geometry.compute_overlap(geometry.bounds(self.current_container), bounds):
@@ -680,7 +680,7 @@ class Designer(DesignPad, Container):
             return
         if event.state & EventMask.CONTROL:
             self.studio.selection.toggle(obj)
-        else:
+        elif obj not in self.studio.selection:
             self.studio.selection.set(obj)
 
     def load(self, obj_class, name, container, attributes, layout, bounds=None):
